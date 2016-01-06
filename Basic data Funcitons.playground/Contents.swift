1a2d8fbe188781059c5aaad46bce77a9c60d6c4c
rsgc-elder-a <aelder@rsgc.on.ca>
2016-01-06 10:49:23 -0500

    This version of my project has the converted floats in the original set of data, I plan to work on the mode function then switch to working on the interface.

--- Basic data Funcitons.playground/Contents.swift
+++ Basic data Funcitons.playground/Contents.swift
@@ -2,13 +2,13 @@
 
 import Cocoa
 
-var data = [6, 5, 50, 10, 60, 30, 40, 50, 70, 10]
-var total = 0
-var count = 0
-var mean = 0
+var data = [6.00, 5.00, 50.00, 10.00, 60.00, 30.00, 40.00, 50.00, 70.00, 10.00, 50.00]
+var total = 0.0
+var count = 0.0
+var mean = 0.0
 
-//mean
-//add up then divde by number
+// mean
+// add up then divde by number
 for values in data {
     total += values
     count = count + 1
@@ -16,14 +16,15 @@ for values in data {
 }
 print(mean)
 
-//median 
-/*use a sorting algorithem to arrange them from greatest to least
-then count down till you get to the middle or middle two numbers
-if there are two numbers add then divide by to otherwise print middle
+// median
+
+/* use a sorting algorithem to arrange them from greatest to least
+ * then count down till you get to the middle or middle two numbers
+ * if there are two numbers add then divide by to otherwise print middle
 */
 
 var countarray = []
-var largest = 0
+var largest = 0.0
 
 // sort the array (using built-in sort)
 data
@@ -33,41 +34,74 @@ sortedData
 // number of elements in array
 var countOfValues = data.count
 
-var median = 0
+var median = 0.0
 
 if(countOfValues % 2 > 0){
     print("odd")
     var middleterm = countOfValues/2
-    print(data[middleterm])
+    median = (data[middleterm])
 } else {
     print("even")
    var middleterm = countOfValues/2
-   median = (data[middleterm] + data[middleterm - 1])/2
+   median = (data[middleterm] + data[middleterm - 1])/2.0
 }
 print(median)
 
-//for values in data {
-//    if(values < largest){
-//        countarray = values
-//    }
-//}
+
 
 
 ////mode
-///*loup over the data count the frqency of all the values print the the 
+///*loup over the data count the freqency of all the values print the the
 //value with the most counts in the second array or dictonary
 //*/
-//
-//var countingarray = []
-//
-//for values in data {
-//    if(value
-//}
-//
-////std devaiton
-////use the data collect and get help from mr. lorrisen about the actual formula8
-//
-//
+
+var countingDictonary = [Double: Int]()
+var count1 = 0.0
+var holdingint = 0.0
+
+
+for values in data {
+    count1 = count1 + 1
+    if(countingDictonary[values] == nil){
+     countingDictonary[values] = 1
+    } else {
+     countingDictonary[values] =  countingDictonary[values]! + 1
+    }
+}
+
+
+
+//need a funciton to sort a diconary???
+
+//countingDictonary
+//var sortedData2 = countingDictonary.sort(Double, Int))
+//countingDictonary
+
+for (value, frequncy) in countingDictonary{
+//use a fucntion to calaulate the highest value in the dictnary
+    if(frequncy > holdingint){
+        print(nice)
+    }
+}
+
+
+//////std devaiton
+//////use the data collect and get help from mr. lorrisen about the actual formula
+////have to use the float to complete this sections of the project
+var submean = []
+var stddevation = 0.0
+for (values) in data {
+    //have to add the values to the arrays
+    submean += (values - mean)*(values - mean)
+    values
+}
+submean
+
+
+
+
 ////z-score
 ///* use the std devation and mean and the pre determined value from the array with the table of values then run it through the value and giver the lower percentage value
-//*/
+//*/I may take this fucntion out i will focos on making the 
+
+
